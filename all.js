bdff880f81d00a79fb2b8bf70edd6ae98bf3aa63
lizongwen <93700983@qq.com>
2018-08-10 11:30:00 +0800

    Initial commit

---
+++ all.js
@@ -0,0 +1,23 @@
+function validate(value) {
+    var pattern = /[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im;
+    if (value === '' || value === null) return false;
+    if (pattern.test(value)) {
+        console.log("非法字符！");
+        return false;
+    }
+    return true;
+}
+function filterSqlStr(value) {
+    var str = "and,delete,or,exec,insert,select,union,update,count,*,',join,>,<";
+    var sqlStr = str.split(',');
+    var flag = true;
+    
+    for (var i = 0; i < sqlStr.length; i++) {
+        if (value.toLowerCase().indexOf(sqlStr[i]) != -1) {
+            flag = false;
+            break;
+        }
+    }
+    console.log(flag);
+    return flag;
+}
